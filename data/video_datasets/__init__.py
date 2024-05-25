e25663d66a49fbc5ae9e827cc50a86359201194e
G-JWLee <101868849+G-JWLee@users.noreply.github.com>
2024-05-26 01:49:16 +0900

    commit

--- data/video_datasets/__init__.py
+++ data/video_datasets/__init__.py
@@ -1,6 +1,7 @@
 from .audioset import AudioSetFinetuneDataset, AudioSetPretrainDataset, AudioSetRetrievalDataset
 from .audioset20k import AudioSet20kFinetuneDataset, AudioSet20kPretrainDataset, AudioSet20kRetrievalDataset
 from .vggsound import VGGSoundFinetuneDataset, VGGSoundPretrainDataset, VGGSoundRetrievalDataset
+from .avs import AVSFinetuneDataset
 from .ave import AVEDataset
 from .msrvtt import MSRVTTRetrievalDataset, MSRVTTPretrainDataset
 from .frames import BaseFinetuneFramesDataset
